6761357fb7a731a2063eaced097601201fa8b572
sohilakaraly340 <sohilakaraly958@gmail.com>
2024-02-27 13:47:06 -0800

    add

--- Node js/controllers/searchServices.js
+++ Node js/controllers/searchServices.js
@@ -1,29 +1,29 @@
 const asyncHandler = require('express-async-handler')
-const productModel =require('../models/productModel')
-const categoryModel=require("../models/categoryModel")
+const productModel = require('../models/productModel')
+const categoryModel = require("../models/categoryModel")
 
 const searchForProduct = asyncHandler(async (req, res) => {
     const name = req.params.name;
-    const productList = await productModel.find({name:{"$regex":name,"$options":"i"}});
-    
+    const productList = await productModel.find({ name: { "$regex": name, "$options": "i" } });
+
     res.status(200).json({ results: productList.length, data: productList });
 })
 const searchForCategory = asyncHandler(async (req, res) => {
     const name = req.params.name;
-    const categoryList = await categoryModel.find({name:{"$regex":name,"$options":"i"}});
+    const categoryList = await categoryModel.find({ name: { "$regex": name, "$options": "i" } });
 
-let productList=[]; 
-let product=[] ;
-for (let index = 0; index < categoryList.length; index++) {
-   console.log(categoryList.length);
-    product.push(await productModel.find({categories:categoryList[index]._id})) 
-}
-console.log(product); 
-// console.log(productList);
+    // let productList = [];
+    let product = [];
+    for (let index = 0; index < categoryList.length; index++) {
+        // console.log(categoryList.length);
+        product.push(await productModel.find({ categories: categoryList[index]._id }))
+    }
+    // console.log(product);
+    // // console.log(productList);
 
 
     res.status(200).json({ results: categoryList.length, data: product });
 })
 
 
-module.exports = {searchForProduct,searchForCategory};
+module.exports = { searchForProduct, searchForCategory };
