b875e29cb81cc4139faa11538f67a9d8e8f1e083
NadaElho <nadaelhosary51@gmail.com>
2024-03-14 19:36:06 +0200

    cart update

--- Angular/AngularE-commerce/src/app/user/userHome/home.component.ts
+++ Angular/AngularE-commerce/src/app/user/userHome/home.component.ts
@@ -84,23 +84,7 @@ export class HomeComponent implements OnInit {
     this.displayedProducts = this.products.slice(startIndex, endIndex);
   }
 
-export class HomeComponent implements OnInit {
-  constructor(private toast: NgToastService, private route: ActivatedRoute) {}
   ngOnInit(): void {
-    if (this.route.snapshot.queryParamMap.get('success')) {
-      this.toast.success({
-        detail: 'SUCCESS',
-        summary: 'Your payment process succeeded',
-        duration: 5000,
-        position: 'topRight',
-      });
-    } else if (this.route.snapshot.queryParamMap.get('canceled')) {
-      this.toast.error({
-        detail: 'ERROR',
-        summary: 'Your payment process failed',
-        duration: 5000,
-        position: 'topRight',
-      });
     if (this.route.snapshot.queryParamMap.get('success')) {
       this.toast.success({
         detail: 'SUCCESS',
