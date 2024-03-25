d45e9afd3054c92357a3afbffe169f150a9722a9
sohilakaraly340 <sohilakaraly958@gmail.com>
2024-03-24 17:52:02 -0700

    nav

--- Node js/middleware/auth.js
+++ Node js/middleware/auth.js
@@ -1,22 +1,24 @@
-const jwt = require('jsonwebtoken')
-const User = require('../models/user.model')
+const jwt = require("jsonwebtoken");
+const User = require("../models/user.model");
 const auth = async (req, res, next) => {
   try {
-    const token = req.headers['jwt']
-    console.log(token)
+    const token = req.headers["jwt"];
+    console.log(token);
     if (!token) {
-      return res.status(401).send({ message: 'un authorized user' })
+      return res.status(401).send({ message: "un authorized user" });
     }
-    const payLoad = await jwt.verify(token, 'myjwtsecret')
-    const { email } = payLoad
-    const user = await User.findOne({ email })
-    if (!user) {
-      return res.status(401).send({ message: 'un authorized user' })
-    }
-    next()
-  } catch (err) {
-    return res.status(401).send({ message: 'un authorized user' })
-  }
-}
 
-module.exports = { auth }
+    const payLoad = jwt.verify(token, "myjwtsecret");
+    console.log("duck");
+    const { email } = payLoad;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).send({ message: "un authorized user" });
+    }
+    next();
+  } catch (err) {
+    return res.status(401).send({ message: "error" });
+  }
+};
+
+module.exports = { auth };
